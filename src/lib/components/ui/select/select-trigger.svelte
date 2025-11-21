43ed7865c7cea2c845a51ce70bcc9a5d6ce8e51d
Jcvdm <vandermerwe.jaco194@gmail.com>
2025-11-21 12:14:56 +0200

    chore: refresh select primitive

--- src/lib/components/ui/select/select-trigger.svelte
+++ src/lib/components/ui/select/select-trigger.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { Select as SelectPrimitive } from "bits-ui";
-	import { ChevronDown } from "lucide-svelte";
+	import ChevronDownIcon from "@lucide/svelte/icons/chevron-down";
 	import { cn, type WithoutChild } from "$lib/utils.js";
 
 	let {
@@ -25,5 +25,5 @@
 	{...restProps}
 >
 	{@render children?.()}
-	<ChevronDown class="size-4 opacity-50" />
+	<ChevronDownIcon class="size-4 opacity-50" />
 </SelectPrimitive.Trigger>
